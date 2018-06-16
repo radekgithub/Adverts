99a510c2eb123110b4c637a3d8f1656639f996b5
Radosław Herbuś <alternatywy2@gmail.com>
2018-06-16 21:44:34 +0200

    User profile form

---
+++ adverts/src/AppBundle/Form/ProfileType.php
@@ -0,0 +1,34 @@
+<?php
+// src/AppBundle/Form/RegistrationType.php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('avatar', FileType::class, array(
+            'label' => 'Choose avatar photo',
+            'data_class' => null));
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
