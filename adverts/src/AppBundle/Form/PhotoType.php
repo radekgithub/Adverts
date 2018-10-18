2ec0fedf33579efcac61d9f2821fce0d16198623
Radosław Herbuś <alternatywy2@gmail.com>
2018-10-18 16:10:42 +0200

    Photo upload form

---
+++ adverts/src/AppBundle/Form/PhotoType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class PhotoType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+//            ->add('photo')
+            ->add('name', FileType::class, array(
+                'label' => 'Choose photo file',
+                'data_class' => null,
+                'mapped'=>false
+            ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Photo'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_advert';
+    }
+
+
+}
